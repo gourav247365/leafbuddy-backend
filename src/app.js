b5dca8c8876b64094600c15eeec98e5789d2db8a
Gourav <gouravnagar2020@gmail.com>
2026-01-12 14:18:37 +0530

    add cors config

--- src/app.js
+++ src/app.js
@@ -11,8 +11,6 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options("*", (req, res) => res.sendStatus(200));
-
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({limit: "16kb"}))

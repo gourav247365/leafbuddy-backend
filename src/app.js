f82749a9bde89a372148c8b641e63993063f17b0
Gourav <gouravnagar2020@gmail.com>
2026-01-12 14:08:45 +0530

    add cors config

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options("*", (req, res) => res.sendStatus(200));
+
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({limit: "16kb"}))
